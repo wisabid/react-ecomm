5b5af651d46544ce464c76ebfb46ca80d9f8dfa6
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-06-01 21:44:13 +0200

    feat(products-listing):integrate

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Checkout from '../Checkout';
+import Products from '../Products';
+import Login from '../Login'
+
+function App() {
+  return (
+    <div className="App">
+      
+        {/* <Checkout /> */}
+        <Products />
+        {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
