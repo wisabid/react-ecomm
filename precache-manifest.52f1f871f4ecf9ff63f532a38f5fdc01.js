ca94887676c237d9b8b932ec19113e41d313f22f
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-06-06 15:49:37 +0200

    Updates

---
+++ precache-manifest.52f1f871f4ecf9ff63f532a38f5fdc01.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd582fd5230170794d0053879e7f0ff",
+    "url": "/react-ecomm/index.html"
+  },
+  {
+    "revision": "bdefa8bd8aeaa69fee11",
+    "url": "/react-ecomm/static/css/main.b43861d6.chunk.css"
+  },
+  {
+    "revision": "fbc7860c53054a5e3084",
+    "url": "/react-ecomm/static/js/2.5af96586.chunk.js"
+  },
+  {
+    "revision": "bdefa8bd8aeaa69fee11",
+    "url": "/react-ecomm/static/js/main.3f846036.chunk.js"
+  },
+  {
+    "revision": "65a599e36f7845b19730",
+    "url": "/react-ecomm/static/js/runtime~main.47b52ead.js"
+  },
+  {
+    "revision": "b0cc591a2cffb75899ae1edbcf7e24a5",
+    "url": "/react-ecomm/static/media/cake.b0cc591a.jpg"
+  },
+  {
+    "revision": "dc40b0d5773202961b67f6fcf4d5b46b",
+    "url": "/react-ecomm/static/media/casual1.dc40b0d5.jpg"
+  },
+  {
+    "revision": "f800898fa24fe44a3a796cd5c2a8511c",
+    "url": "/react-ecomm/static/media/cotton.f800898f.jpg"
+  },
+  {
+    "revision": "1d22027b99959295a4510a03ce7720f1",
+    "url": "/react-ecomm/static/media/flip-flop1.1d22027b.jpg"
+  },
+  {
+    "revision": "8ff1df62af812110b1406422ee4c0dcb",
+    "url": "/react-ecomm/static/media/loafers1.8ff1df62.jpg"
+  },
+  {
+    "revision": "1df7554fa012b04fda63fcbb819ff694",
+    "url": "/react-ecomm/static/media/nakd.1df7554f.jpg"
+  },
+  {
+    "revision": "3b009c3691e9c40a2905b65228a64b53",
+    "url": "/react-ecomm/static/media/nike.3b009c36.jpeg"
+  },
+  {
+    "revision": "e5cd63ac9e9be2e1a03f60fa9c328d79",
+    "url": "/react-ecomm/static/media/patch.e5cd63ac.jpg"
+  },
+  {
+    "revision": "6e07ebe0f7a83926ccdba62126393b61",
+    "url": "/react-ecomm/static/media/shoes1.6e07ebe0.JPG"
+  },
+  {
+    "revision": "77f6e51993751163e4b716508667d766",
+    "url": "/react-ecomm/static/media/shoes2.77f6e519.jpg"
+  },
+  {
+    "revision": "0ccafd9ea0fe253ce1f4204cb5eb999d",
+    "url": "/react-ecomm/static/media/stripe-green.0ccafd9e.jpg"
+  },
+  {
+    "revision": "ce46daaa46f2dbbade2bad334e6d3f16",
+    "url": "/react-ecomm/static/media/stripe.ce46daaa.jpg"
+  },
+  {
+    "revision": "b104298a62161d2fb98224d45aa2b932",
+    "url": "/react-ecomm/static/media/waste.b104298a.jpg"
+  }
+]);
